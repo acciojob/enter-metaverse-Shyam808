0626786afade16a187ef19acafc48a84b06c4f1d
Shyam Paghadal <shyampaghadal@gmail.com>
2025-06-21 22:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("enterBtn").addEventListener("click", () => {
   const statusElement = document.getElementById("status");
 
   // Replace <p> with <h1> while preserving the ID and changing the text
-  const h1 = document.createElement("h1");
-  h1.id = "status";
-  h1.textContent = "Entered Metaverse";
+  const p = document.createElement("h1");
+  p.id = "status";
+  p.textContent = "Entered Metaverse";
 
   // Replace the original element
   statusElement.replaceWith(h1);
